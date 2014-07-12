990da058e089de17b63d64b0e15667a9c854365d
avesther <avesther0@gmail.com>
2014-07-12 16:22:29 +0200

    controller, controllertest

---
+++ initial/src/main/java/com/yummynoodlebar/web/controller/SimpleSiteController.java
@@ -0,0 +1,9 @@
+package com.yummynoodlebar.web.controller;
+
+/**
+ * Created by Avesther on 7/12/2014.
+ */
+public class SimpleSiteController {
+
+
+}
---
+++ initial/src/test/java/com/yummynoodlebar/web/controller/SiteIntegrationTest.java
@@ -0,0 +1,7 @@
+package com.yummynoodlebar.web.controller;
+
+/**
+ * Created by Avesther on 7/12/2014.
+ */
+public class SiteIntegrationTest {
+}
